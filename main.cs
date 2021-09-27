4b2f9bbb2a14b38d23f85c28777f56809a97065a
Tony Murray <85173110+Murray-Tony@users.noreply.github.com>
2021-09-27 00:00:14 +0000

    fixed issue with larger numbers

--- main.cs
+++ main.cs
@@ -5,31 +5,23 @@ class Program {
     Console.WriteLine ("Please provide a number:");
     double number = Math.Abs(Convert.ToDouble(Console.ReadLine()));
     int prime = 0;
-    if (number == 2)
-    {
-      Console.WriteLine("Your number is prime.");
-    }
     if (number <= 1)
     {
       Console.WriteLine("Your number is not prime.");
+      prime++;
     }
     for (int divisor = 2; divisor < number; divisor++)
     {
       double remainder = number % divisor;
       if (remainder == 0) 
       {Console.WriteLine("Your number is not prime.");
+      prime++;
       break;
-      }
-      else
-      {
-        prime++; 
-      }
-      
+      }  
     }
-  if (prime > 0)
-  {
-    Console.WriteLine("Your number is prime.");
-  }
-
+    if (prime == 0)
+    {
+      Console.WriteLine("Your number is prime.");
+    }
   }
 }
